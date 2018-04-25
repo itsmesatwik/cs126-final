6fec54fb1c82339e7843d78f22b639fe1d0d3e1a
satwik <satwik.illini@gmail.com>
2018-04-25 14:29:56 -0500

    Added pillar

--- flappy-bird/src/Pillar.h
+++ flappy-bird/src/Pillar.h
@@ -5,7 +5,7 @@ class Pillar {
 private:
     ofRectangle pillar_;
     ofVec2f screen_dim_;
-    static const float size_ratio_;
+    static const double size_ratio_;
     ofVec2f pillar_size_;
     ofVec2f pillar_pos_;
     
@@ -13,7 +13,8 @@ public:
     Pillar();
     ~Pillar();
     ofVec2f getPillarSize() const;
+    ofVec2f getPillarPos() const;
     void update();
     void resize(int w, int h);
-    
+    ofRectangle getRect() const;
 };
